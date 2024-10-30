f352988e90995047445e355923e2b3f0502ec7a2
ThangTNHE186465 <129970936+ThangTNHE186465@users.noreply.github.com>
2024-10-31 04:17:02 +0700

    update models

--- HRM/Models/UserSession.cs
+++ HRM/Models/UserSession.cs
@@ -1,16 +1,32 @@
-﻿namespace HRM.Models;
+﻿using HRM.Service;
+using HRM.Service.ServiceImpl;
+
+namespace HRM.Models;
 
 public class UserSession
 {
-    private static UserSession _instance;
+    private IEmployeeService _employeeService = new EmployeeService();
+    private static UserSession? _instance;
     public static UserSession Instance => _instance ??= new UserSession();
 
-    public User User { get; private set; }
+    public User? User { get; private set; }
+    public Employee? Employee { get; set; }
 
     private UserSession() { }
 
-    public void SetUser(User user)
+    public async Task SetUser(User? user)
     {
         User = user;
-    }  
+        Employee = await _employeeService.GetByUserId(User.Id);
+    }
+
+    public async Task<Employee> GetEmployee()
+    {
+        return Employee;
+    }
+
+    public void Clear()
+    {
+        User = null;
+    }
 }
