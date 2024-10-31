9034809a02f7c080fca805f8e1b0557dde2aa63d
AnhVVHE186936 <anhvvhe186936@fpt.edu.vn>
2024-10-31 21:42:55 +0700

    modify AdminViewModel

--- HRM/ViewModels/Admin/AdminViewModel.cs
+++ HRM/ViewModels/Admin/AdminViewModel.cs
@@ -4,79 +4,80 @@ using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using HRM.Models;
 using HRM.Service;
 using HRM.Service.ServiceImpl;
 
-namespace HRM.ViewModels.Admin;
+namespace HRM.ViewModels;
 
 public partial class AdminViewModel : ObservableObject
 {
     private readonly INavigationService _navigationService;
     private readonly IAuthService _authService;
-    // private readonly IUserService _userService;
+    private readonly IEmployeeService _employeeService;
 
-    [ObservableProperty] private string _adminName;
-    [ObservableProperty] private BitmapImage _adminAvatar;
+    [ObservableProperty] private string? _adminName;
+    [ObservableProperty] private BitmapImage? _adminAvatar;
     [ObservableProperty] private bool _isLoading;
 
-    public AdminViewModel()
-    {
-    }
     public AdminViewModel(Frame frame)
     {
         _navigationService = new NavigationService(frame);
         _authService = new AuthService();
-        // _userService = userService;
+        _employeeService = new EmployeeService();
 
         // Initialize commands
         NavigateCommand = new RelayCommand<string>(ExecuteNavigate);
         ProfileCommand = new RelayCommand(ExecuteProfile);
-        LogoutCommand = new RelayCommand(ExecuteLogout);
 
         // Load admin info
-        // LoadAdminInfo();
+        LoadAdminInfo();
     }
 
     public ICommand NavigateCommand { get; }
     public ICommand ProfileCommand { get; }
-    public ICommand LogoutCommand { get; }
+    public ICommand LogoutCommand { get; set; }
 
     private void ExecuteNavigate(string? destination)
     {
         switch (destination)
         {
+            case "Home":
+                _navigationService.NavigateTo("AdminDashboard");
+                break;
             case "EmployeeManagement":
                 _navigationService.NavigateTo("EmployeeListView");
                 break;
-            // case "DepartmentManagement":
-            //     _navigationService.NavigateTo();
-            //     break;
-            // case "PayrollManagement":
-            //     _navigationService.NavigateTo();
-            //     break;
-            // case "Statistics":
-            //     _navigationService.NavigateTo();
-            //     break;
+            case "DepartmentManagement":
+                _navigationService.NavigateTo("DepartmentListView");
+                break;
+            case "SalaryManagement":
+                _navigationService.NavigateTo("SalaryManagementView");
+                break;
+            case "Statistics":
+                _navigationService.NavigateTo("ReportView");
+                break;
         }
     }
 
     private void ExecuteProfile()
     {
-        // _navigationService.NavigateTo<AdminProfileView>();
+        _navigationService.NavigateTo("UserProfileView");
     }
 
-    private async void ExecuteLogout()
+    private async void LoadAdminInfo()
     {
         try
         {
             IsLoading = true;
-            await _authService.LogoutAsync(1);
-            _navigationService.NavigateToLogin();
+            var currentEmp = UserSession.Instance.Employee;
+            AdminName = currentEmp.FullName;
+            AdminAvatar = await LoadAvatarImage(currentEmp.PhotoPath);
         }
         catch (Exception ex)
         {
-            // Handle logout error
-            ShowError("Đăng xuất thất bại", ex.Message);
+            // Handle loading error
+            ShowError("Lỗi tải thông tin", ex.Message);
         }
         finally
         {
@@ -84,30 +85,10 @@ public partial class AdminViewModel : ObservableObject
         }
     }
 
-    // private async void LoadAdminInfo()
-    // {
-    //     try
-    //     {
-    //         IsLoading = true;
-    //         var currentUser = await _userService.GetCurrentUserAsync();
-    //         AdminName = currentUser.FullName;
-    //         AdminAvatar = await LoadAvatarImage(currentUser.AvatarUrl);
-    //     }
-    //     catch (Exception ex)
-    //     {
-    //         // Handle loading error
-    //         ShowError("Lỗi tải thông tin", ex.Message);
-    //     }
-    //     finally
-    //     {
-    //         IsLoading = false;
-    //     }
-    // }
-
-    private async Task<BitmapImage> LoadAvatarImage(string avatarUrl)
+    private async Task<BitmapImage?> LoadAvatarImage(string avatarUrl)
     {
         if (string.IsNullOrEmpty(avatarUrl))
-            return new BitmapImage(new Uri("pack://application:,,,/Assets/default-avatar.png"));
+            return new BitmapImage(new Uri("pack://application:,,,/Assets/avatar/default.jpg"));
 
         try
         {
@@ -120,7 +101,7 @@ public partial class AdminViewModel : ObservableObject
         }
         catch
         {
-            return new BitmapImage(new Uri("pack://application:,,,/Assets/default-avatar.png"));
+            return new BitmapImage(new Uri("pack://application:,,,/Assets/avatar/default.jpg"));
         }
     }
 
