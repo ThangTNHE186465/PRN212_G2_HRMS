04468bcb295b52f0d4ca681a9e7ac751120fe1da
ThangTNHE186465 <129970936+ThangTNHE186465@users.noreply.github.com>
2024-11-05 02:10:02 +0700

    add login screen

---
+++ WPFApp/LoginScreen.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+using BusinessObjects;
+using Repositories;
+
+namespace WPFApp
+{
+    public partial class LoginScreen : Window
+    {
+        private readonly AccountRepository _accountRepository;
+        public LoginScreen()
+        {
+            InitializeComponent();
+            _accountRepository = new AccountRepository();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUsername != null && txtPassword != null)
+            {
+                var account = _accountRepository.GetAccountByUserName(txtUsername.Text);
+                if (account != null)
+                {
+                    if (account.Password.Equals(txtPassword.Password))
+                    {
+                        SessionManager.CurrentAccount = account;
+                        var employee = _accountRepository.GetEmployeeByUsername(account.AccountId);
+                        if (account.Role.RoleName.Equals("Admin"))
+                        {
+                            AdminDashboard adminDashboard = new AdminDashboard();
+                            adminDashboard.Show();
+                            this.Close();
+                        }
+                        else if (account.Role.RoleName.Equals("Employee"))
+                        {
+                            HomeEmployee homeEmployee = new HomeEmployee();
+                            homeEmployee.Show();
+                            this.Close();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Incorrect password");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect username");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter username and password");
+            }
+        }
+    }
+
+    public static class SessionManager
+    {
+        public static Account CurrentAccount { get; set; }
+    }
+}
