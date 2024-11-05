8f3898043e8d10d537d1ac2284eea6be0f79d315
AnhVVHE186936 <anhvvhe186936@fpt.edu.vn>
2024-11-05 12:02:57 +0700

    Add Leave Request for Admin

---
+++ WPFApp/AdminViews/LeaveRequestView.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using BusinessObjects;
+using DataAccessObjects;
+using Repositories;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Interaction logic for LeaveRequestView.xaml
+    /// </summary>
+    public partial class LeaveRequestView : Window
+    {
+        private readonly LeaveRequestRepository _leaveRepository;
+        public LeaveRequestView()
+        {
+            InitializeComponent();
+            _leaveRepository = new LeaveRequestRepository();
+            LoadLeaveRequest();
+        }
+        public void LoadLeaveRequest()
+        {
+            var leaveRequest = _leaveRepository.getAllLeaveRequest();
+            LeaveRequestDataGrid.ItemsSource = leaveRequest;
+        }
+        public void LeaveRequestDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (LeaveRequestDataGrid.SelectedItem is LeaveRequest selectedLeave)
+            {
+                int leaveRequestID = selectedLeave.LeaveRequestId;
+                LeaveRequest leaveRequestDetail = _leaveRepository.getLeaveRequest(leaveRequestID);
+                if (leaveRequestDetail != null) {
+                    EmployeeNameText.Text = leaveRequestDetail.Employee.FullName;
+                    DepartmentNameText.Text = leaveRequestDetail.Employee.Department.DepartmentName;
+                    LeaveTypeText.Text = leaveRequestDetail.LeaveType.ToString();
+                    StartDateText.Text = leaveRequestDetail.StartDate.ToString();
+                    EndDateText.Text = leaveRequestDetail.EndDate.ToString();
+                    StatusText.Text = leaveRequestDetail.Status.ToString();
+
+                }
+            }
+        }
+        private void ChangeStatus_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (LeaveRequestDataGrid.SelectedItem is LeaveRequest selectedLeave)
+            {
+                int leaveRequestID = selectedLeave.LeaveRequestId;
+                string newStatus = button.Content.ToString(); // Lấy nội dung của nút (Approved hoặc Rejected)
+                int leaveRequestId = leaveRequestID;
+                var dao = new LeaveRequestDAO();
+
+                try
+                {
+                    dao.ChangeStatus(leaveRequestId, newStatus);
+                    MessageBox.Show($"Status changed to {newStatus}");
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
