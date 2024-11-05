6dfd2ba3549bb69108eaddf0dff2bb3606bf260b
huyn1234-del <huynqhe180360@fpt.edu.vn>
2024-11-05 09:52:19 +0700

    add ui and logic for employee

---
+++ WPFApp/EmployeeView/HomeEmployee.xaml.cs
@@ -0,0 +1,56 @@
+﻿using BusinessObjects;
+using DataAccessObjects;
+using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFApp
+{
+    public partial class HomeEmployee : Window
+    {
+        EmployeeRepository employeeRepository;
+        public HomeEmployee()
+        {
+            InitializeComponent();
+            FuhrmContext context = new FuhrmContext();
+            EmployeeDAO ed = new EmployeeDAO(context);
+            employeeRepository = new EmployeeRepository(ed);
+            LoadEmployeeData();
+        }
+
+        private void LoadEmployeeData()
+        {
+            var currentAccount = SessionManager.CurrentAccount;
+            if (currentAccount != null)
+            {
+                var em = employeeRepository.GetEmployeeByAccountId(currentAccount.AccountId);
+                WelcomeTextBlock.Text = $"Xin chào {em.FullName}";
+                NameTextBox.Text = em.FullName;
+                BirthTextBox.Text = em.DateOfBirth.ToString(CultureInfo.CurrentCulture).Split(" ")[0];
+                GenderTextBox.Text = em.Gender;
+                AddressTextBox.Text = em.Address;
+                PhoneTextBox.Text = em.PhoneNumber;
+                StartTextBox.Text = em.StartDate.ToString(CultureInfo.InvariantCulture).Split(" ")[0];
+                DepartmentTextBox.Text = em.Department.DepartmentName;
+                PositionTextBox.Text = em.Position.PositionName;
+                SalaryTextBox.Text = em.Salary.ToString(CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                WelcomeTextBlock.Text = "Xin chào nhân viên";
+            }
+        }
+    }
+}
---
+++ WPFApp/EmployeeView/TakeAttendance.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Repositories;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using BusinessObjects;
+using DataAccessObjects;
+
+namespace WPFApp
+{
+    public partial class TakeAttendance : Window
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IAttendanceRepository _attendanceRepository;
+        private readonly int _accountId;
+
+        public TakeAttendance(int accountId)
+        {
+            InitializeComponent();
+            var context = new FuhrmContext();
+            _employeeRepository = new EmployeeRepository(new EmployeeDAO(context));
+            _attendanceRepository = new AttendanceRepository();
+            _accountId = accountId;
+            LoadEmployeeDetails();
+        }
+
+        private void LoadEmployeeDetails()
+        {
+            var employee = _employeeRepository.GetAllEmployees().FirstOrDefault(e => e.AccountId == _accountId);
+            if (employee != null)
+            {
+                EmployeeIdTextBox.Text = employee.EmployeeId.ToString();
+                EmployeeNameTextBox.Text = employee.FullName;
+                AttendanceDateTextBox.Text = DateTime.Now.ToString("yyyy-MM-dd");
+                StatusTextBox.Text = "Có mặt";
+            }
+            else
+            {
+                MessageBox.Show("Không tìm thấy nhân viên.", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+                Close();
+            }
+        }
+
+        private void SaveAttendanceButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var attendance = new Attendance
+                {
+                    EmployeeId = int.Parse(EmployeeIdTextBox.Text),
+                    Date = DateOnly.FromDateTime(DateTime.Now),
+                    Status = "Có mặt"
+                };
+                _attendanceRepository.AddAttendance(attendance);
+                MessageBox.Show("Chấm công thành công.", "Thành công", MessageBoxButton.OK, MessageBoxImage.Information);
+                HomeEmployee homeEmployee = new HomeEmployee();
+                homeEmployee.Show();
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khi chấm công: {ex.Message}", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
