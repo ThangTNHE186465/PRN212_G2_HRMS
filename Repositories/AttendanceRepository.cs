a2d3fd4fd09a66e22301c8b149d2972d5f3cc34f
huyn1234-del <huynqhe180360@fpt.edu.vn>
2024-11-05 09:50:13 +0700

    modify repository

--- Repositories/AttendanceRepository.cs
+++ Repositories/AttendanceRepository.cs
@@ -21,23 +21,4 @@ namespace Repositories
         public List<Attendance> GetAttendances()
             => AttendanceDAO.GetAttendances();
     }
-
-    public class AttendanceRepository : IAttendanceRepository
-    {
-        public void AddAttendance(Attendance attendance)
-            => AttendanceDAO.AddAttendance(attendance);
-
-        public List<Attendance>? SearchAttendance(string employeeName)
-            => AttendanceDAO.SearchAttendance(employeeName);
-
-        public void RemoveAttendance(Attendance attendance)
-            => AttendanceDAO.RemoveAttendance(attendance);
-
-        public void UpdateAttendance(Attendance attendance)
-            => AttendanceDAO.UpdateAttendance(attendance);
-
-        public List<Attendance> GetAttendances()
-            => AttendanceDAO.GetAttendances();
-    }
-}
 }
--- Repositories/IAccountRepository.cs
+++ Repositories/IAccountRepository.cs
@@ -14,7 +14,7 @@ namespace Repositories
         public void AddAccount(Account account);
         public void UpdateAccount(Account account);
         public void DeleteAccount(int accountId);
-        public Account GetAccountById(int accountId); 
+        public Account GetAccountById(int accountId);
         public Account GetAccountByUserName(string name);
 
         public Account GetAccountByEmployeeId(int employeeId);
